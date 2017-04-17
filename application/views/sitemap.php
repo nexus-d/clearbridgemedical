45f21124574093f5793480a0fc93066aa7ca51e7
Denis Gutierrez <infotech.ench.sho28@gmail.com>
2017-04-17 22:16:53 +0800

    sitemap.php

---
+++ application/views/sitemap.php
@@ -0,0 +1,79 @@
+<?php echo'<?xml version="1.0" encoding="UTF-8" ?>' ?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+
+<url>
+  <loc>http://cbmsite.byethost7.com</loc>
+  <lastmod>2017-04-10T04:45:41+00:00</lastmod>
+  <changefreq>always</changefreq>
+</url>
+<url>
+  <loc>http://cbmsite.byethost7.com/home</loc>
+  <lastmod>2017-04-10T04:45:41+00:00</lastmod>
+  <changefreq>always</changefreq>
+</url>
+<url>
+  <loc>http://cbmsite.byethost7.com/about-us</loc>
+  <lastmod>2017-04-10T04:45:41+00:00</lastmod>
+  <changefreq>always</changefreq>
+</url>
+<url>
+  <loc>http://cbmsite.byethost7.com/wellness</loc>
+  <lastmod>2017-04-10T04:45:41+00:00</lastmod>
+  <changefreq>always</changefreq>
+</url>
+<url>
+  <loc>http://cbmsite.byethost7.com/fitness</loc>
+  <lastmod>2017-04-10T04:45:41+00:00</lastmod>
+  <changefreq>always</changefreq>
+</url>
+<url>
+  <loc>http://cbmsite.byethost7.com/types-of-test</loc>
+  <lastmod>2017-04-10T04:45:41+00:00</lastmod>
+  <changefreq>always</changefreq>
+</url>
+<url>
+  <loc>http://cbmsite.byethost7.com/types-of-test/metabolomic-test</loc>
+  <lastmod>2017-04-10T04:45:41+00:00</lastmod>
+  <changefreq>always</changefreq>
+</url>
+<url>
+  <loc>http://cbmsite.byethost7.com/types-of-test/metabolic-test</loc>
+  <lastmod>2017-04-10T04:45:41+00:00</lastmod>
+  <changefreq>always</changefreq>
+</url>
+<url>
+  <loc>http://cbmsite.byethost7.com/types-of-test/our-programs</loc>
+  <lastmod>2017-04-10T04:45:41+00:00</lastmod>
+  <changefreq>always</changefreq>
+</url>
+<url>
+  <loc>http://cbmsite.byethost7.com/contact-us/</loc>
+  <lastmod>2017-04-10T04:45:41+00:00</lastmod>
+  <changefreq>always</changefreq>
+</url>
+<url>
+  <loc>http://cbmsite.byethost7.com/contact-us/e-appointment</loc>
+  <lastmod>2017-04-10T04:45:41+00:00</lastmod>
+  <changefreq>always</changefreq>
+</url>
+<url>
+  <loc>http://cbmsite.byethost7.com/contact-us/enquiry</loc>
+  <lastmod>2017-04-10T04:45:41+00:00</lastmod>
+  <changefreq>always</changefreq>
+</url>
+<url>
+  <loc>http://cbmsite.byethost7.com/contact-us/mailing-address</loc>
+  <lastmod>2017-04-10T04:45:41+00:00</lastmod>
+  <changefreq>always</changefreq>
+</url>
+    <url>
+  <loc>http://cbmsite.byethost7.com/contact-us/operating-hours</loc>
+  <lastmod>2017-04-10T04:45:41+00:00</lastmod>
+  <changefreq>always</changefreq>
+</url>
+</urlset>
