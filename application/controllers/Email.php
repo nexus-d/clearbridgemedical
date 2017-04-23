e035a7fc5f7f1b052af3c49353aae6349f499770
Denis Gutierrez <infotech.ench.sho28@gmail.com>
2017-04-23 12:07:51 +0800

    Email Functions

--- application/controllers/Email.php
+++ application/controllers/Email.php
@@ -7,69 +7,7 @@ class Email extends CORE_Controller {
     public function index() {
     }
   
-    function sendhmail(){
-
-            $content="hello world";
-            $this->load->library('email');
-            $email_setting  = array('mailtype'=>'html');
-            $this->email->initialize($email_setting);
-            $this->email->from('denis.insightmedica@gmail.com', 'Denis Gutierrez');
-            $this->email->to('infotechnexus28@gmail.com');
-            //$this->email->cc('another@another-example.com');
-            //$this->email->bcc('them@their-example.com');
-            $this->email->subject('Request for Appointment ');
-            $this->email->message('$content');
-            //$this->email->set_mailtype('html');
-            if($this->email->send()){
-
-            $response['title']='Sent';
-            $response['stat']='success';
-            $response['msg']='Email successfully sent.';
-            echo json_encode($response);
-            }else{
-            $response['title']='Failed';
-            $response['stat']='error';
-            $response['msg']='Email successfully not sent.';
-            echo json_encode($response);
-            }
-
-    }
-
-
-
-    function sendgmail(){
-            $config = Array(
-            'protocol' => 'smtp',
-            'smtp_host' => 'ssl://smtp.gmail.com',
-            'smtp_port' => 465,
-            'smtp_user' => 'denis.insightmedica@gmail.com',
-            'smtp_pass' => '!Admin123456',
-            'mailtype'  => 'html', 
-            'charset'   => 'iso-8859-1'
-            );
-            $this->load->library('email', $config);
-            $this->email->set_newline("\r\n");
-            $this->email->from('denis.insightmedica@gmail.com', 'Denis Gutierrez');
-            $this->email->to('infotechnexus28@gmail.com');
-            $this->email->subject('Request for Appointment ');
-            $this->email->message('Hello World');
-            if($this->email->send()){
-            $response['title']='Sent';
-            $response['stat']='success';
-            $response['msg']='Email successfully sent.';
-            echo json_encode($response);
-            }else{
-            $response['title']='Failed';
-            $response['stat']='error';
-            $response['msg']='Email successfully not sent.';
-            echo json_encode($response);
-            }
-
-    }
-       
-        function test(){
-
-        $this->load->library('email');
+    function sendmail(){
 
         $subject = 'This is a test';
         $message = '<p>This message has been sent for testing purposes.</p>';
@@ -91,58 +29,40 @@ class Email extends CORE_Controller {
         ' . $message . '
         </body>
         </html>';
-        // Also, for getting full html you may use the following internal method:
-        //$body = $this->email->full_html($subject, $message);
-
-        $result = $this->email
-        ->from('denis.insightmedica@gmail.com')
-        ->to('infotechnexus28@gmail.com')
-        ->subject($subject)
-        ->message($body)
-        ->send();
-
-        var_dump($result);
-        echo '<br />';
-        echo $this->email->print_debugger();
-        exit;
-        }
 
 
 
+            $content="hello world";
+            $this->load->library('email');
+            $email_setting  = array('mailtype'=>'html');
+            $this->email->initialize($email_setting);
+            $this->email->from('denis.insightmedica@gmail.com', 'Denis Gutierrez');
+            $this->email->to('infotechnexus28@gmail.com');
+            //$this->email->cc('another@another-example.com');
+            //$this->email->bcc('them@their-example.com');
+            $this->email->subject($subject);
+            $this->email->message($body);
+            //$this->email->set_mailtype('html');
+            if($this->email->send()){
 
-        public function send_mail()
-        {
-        require_once(APPPATH.'third_party/phpmailer/PHPMailerAutoload.php');
-        $mail = new PHPMailer();
-        $mail->IsSMTP(); // we are going to use SMTP
-        $mail->SMTPAuth   = true; // enabled SMTP authentication
-        $mail->SMTPSecure = "ssl";  // prefix for secure protocol to connect to the server
-        $mail->Host       = "smtp.gmail.com";      // setting GMail as our SMTP server
-        $mail->Port       = 465;                   // SMTP port to connect to GMail
-        $mail->Username   = "denis.insightmedica@gmail.com";  // user email address
-        $mail->Password   = "!Admin123456";            // password in GMail
-        $mail->SetFrom('denis.insightmedica@gmail.com', 'Denis Gutierrez');  //Who is sending 
-        $mail->isHTML(true);
-        $mail->Subject    = "Mail Subject";
-        $mail->Body      = '
-        <html>
-        <head>
-        <title>Title</title>
-        </head>
-        <body>
-        <h3>Heading</h3>
-        <p>Message Body</p><br>
-        <p>With Regards</p>
-        <p>Your Name</p>
-        </body>
-        </html>
-        ';
-        $destino = 'denis.insightmedica@gmail.com'; // Who is addressed the email to
-        $mail->AddAddress($destino, "Receiver");
-        if(!$mail->Send()) {
-        return false;
-        } else {
-        return true;
-        }
-        }
+            $response['title']='Sent';
+            $response['stat']='success';
+            $response['msg']='Email successfully sent.';
+            echo json_encode($response);
+            }else{
+            $response['title']='Failed';
+            $response['stat']='error';
+            $response['msg']='Email successfully not sent.';
+            echo json_encode($response);
+            }
+
+    }
+
+
+
+    
+        
+
+
+
 }
--- application/views/files/pages/mailing_address.php
+++ application/views/files/pages/mailing_address.php
@@ -13,7 +13,72 @@
     <title><?php echo $title;?></title>
     
     <?php echo $sub_view_css_files; ?>
+ <style>
+           /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height:500px;
+      }
+      /* Optional: Makes the sample page fill the window. */
+   
+      #floating-panel {
+        position: relative;
+        top: 10px;
+        left: 25%;
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+        text-align: center;
+        font-family: 'Roboto','sans-serif';
+        line-height: 30px;
+        padding-left: 10px;
+      }
+      #right-panel {
+        font-family: 'Roboto','sans-serif';
+        line-height: 30px;
+        padding-left: 10px;
+      }
 
+      #right-panel select, #right-panel input {
+        font-size: 15px;
+      }
+
+      #right-panel select {
+        width: 100%;
+      }
+
+      #right-panel i {
+        font-size: 12px;
+      }
+      #right-panel {
+        height: 500px;
+        float: right;
+        width: 390px;
+        overflow: auto;
+      }
+     
+      #floating-panel {
+        background: #fff;
+        padding: 5px;
+        font-size: 14px;
+        font-family: Arial;
+        border: 1px solid #ccc;
+        box-shadow: 0 2px 2px rgba(33, 33, 33, 0.4);
+        display: none;
+      }
+      @media print {
+        #map {
+          height: 500px;
+          margin: 0;
+        }
+        #right-panel {
+          float: none;
+          width: auto;
+        }
+      }
+    
+    </style>
 </head>
 
 <body>
@@ -67,54 +132,26 @@
                 </div>
                 
                 <br/>
-                
-                <!-- Replace Latitude, Logitude and Info Window as per your place -->
-                <div class="gmap"
-                     style="height:300px;" 
-                     data-lat="1.3487963" 
-                     data-long="103.8408780"
-                     data-info-win="<strong>Clearbridge Medical Centre</strong><br/><span>37 Jalan Pemimpin
-#08-05 Mapex
-Singapore 577177</span>">
                 </div>
-            </div>
-<!--
-            <div class="col-sm-6 mt-20">
-                <div class="focus-box-1">
-                    <address>
-                        <strong>Australia Perth Office</strong><br>
-                       Clearbridge Medical Centre
-                        <br>
-                    </address>
-                    <ul class="contact-list">
-                    <li>
-                        <i class="fa fa-map-marker"></i>Level 10, 167 Queen Street  Melbourne VIC 3000 Australia
-                    </li>
-                       
-
-                    <li>
-                        <i class="fa fa-phone-square"></i> Main Line: +61 8 6102 0938
-                         <br/>
-                          <br/>
-                           <br/> <br/>
-                    </li>
-                   
-                        
-                    </ul>
-                </div>
-                
-                <br/>
-                
-    
-                <div class="gmap"
-                     style="height:300px;" 
-                     data-lat="-37.814200" 
-                     data-long="144.960195"
-                     data-info-win="<strong>Clearbridge Medical Centre</strong><br/><span>Level 10, 167 Queen Street Melbourne VIC 3000 Australia</span>">
-                </div>
+        <div class="col-sm-8">
+            <div id="floating-panel">
+                <b>Mode of Travel: </b>
+                <select id="mode">
+                <option value="DRIVING">Driving</option>
+                <option selected="selected" value="WALKING">Walking</option>
+                <option value="TRANSIT">Transit</option>
+                </select>
             </div>
 
-    -->        
+            <div id="map"></div>
+
+
+        </div>
+        <div class="col-sm-4">
+           <div id="right-panel"></div> 
+        </div>
+    </div>
+        
     </div>
   </div>  
 
@@ -124,7 +161,58 @@ Singapore 577177</span>">
       <?php echo $gcse_modal; ?>
     <?php echo $sub_view_js_files; ?>
 
-     <script src="http://maps.google.com/maps/api/js?key=AIzaSyB4XUPGA9TMbCGypqWidsj-fx69wrxBuRU" type="text/javascript"></script>
+  <script>
+      function initMap() {
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var directionsService = new google.maps.DirectionsService;
+        var map = new google.maps.Map(document.getElementById('map'), {
+           zoom: 14,
+          center: {lat: 1.3487963, lng: 103.8408780}
+
+        });
+        directionsDisplay.setMap(map);
+        directionsDisplay.setPanel(document.getElementById('right-panel'));
+
+        var control = document.getElementById('floating-panel');
+        control.style.display = 'block';
+        map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+
+        var onChangeHandler = function() {
+          calculateAndDisplayRoute(directionsService, directionsDisplay);
+        };
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+        document.getElementById('mode').addEventListener('change', onChangeHandler);
+      }
+
+      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+
+   if (navigator.geolocation) { //Checks if browser supports geolocation
+      navigator.geolocation.getCurrentPosition(function (position) {                                                              //This gets the
+      var latitude = position.coords.latitude;                    //users current
+      var longitude = position.coords.longitude;                 //location
+      var coords = new google.maps.LatLng(latitude, longitude); //Creates variable for map coordinates
+      var mode =  document.getElementById('mode').value;
+
+        directionsService.route({
+          origin: coords,  // Current Location.
+          destination: {lat: 1.3487963, lng: 103.8408780},
+          travelMode: mode
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+
+   });
+ }
+
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB4XUPGA9TMbCGypqWidsj-fx69wrxBuRU&callback=initMap">
+    </script>
    
 </body>
 </html>
